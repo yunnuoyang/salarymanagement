cfd73d9495a0987c2fa7a20056e7bcc72cd0f242
yunnuoyang <50757618+yunnuoyang@users.noreply.github.com>
2020-03-09 18:01:42 +0800

    跨域解决方案

---
+++ src/main/java/com/joinx/CrosConfiguration.java
@@ -0,0 +1,18 @@
+package com.joinx;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrosConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
